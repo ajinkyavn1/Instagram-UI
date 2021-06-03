c246f829b25f315d3ffa88416c8d5eb66e7c9030
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-03 17:08:29 +0530

    Update Insta_List.dart

--- lib/Insta_List.dart
+++ lib/Insta_List.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instaui/Insta_Stories.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class InstaList extends StatelessWidget {
 
@@ -13,7 +14,37 @@ class InstaList extends StatelessWidget {
           height: deviceSize.height* 0.14,
             child: InstaStories()
         )
-            :Column(),
+            :
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+              Padding(
+                child: Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                          image: NetworkImage("https://avatars.githubusercontent.com/u/46190988?v=4")
+                      )
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  "Ajinkya Narkhede".text.bold.make()
+                ],
+            ),
+              ),
+            Icon(Icons.more_vert)
+
+          ],
+        ),
     );
   }
 }
