63e52dcf1bbb399774058df4c5032224e03f0cd9
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-03 17:36:32 +0530

    updated a backgrounf comment

--- lib/Insta_List.dart
+++ lib/Insta_List.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instaui/Insta_Stories.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 
 class InstaList extends StatelessWidget {
 
@@ -21,28 +24,57 @@ class InstaList extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
               Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16,8,16),
                 child: Row(
-                children: [
-                  Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                          image: NetworkImage("https://avatars.githubusercontent.com/u/46190988?v=4")
-                      )
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage("https://avatars.githubusercontent.com/u/46190988?v=4")
+                              )
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        "Ajinkya Narkhede".text.bold.make()
+                      ],
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  "Ajinkya Narkhede".text.bold.make()
-                ],
-            ),
+                    Icon(Icons.more_vert)
+                  ],
+                ),
               ),
-            Icon(Icons.more_vert)
+             Flexible(
+               fit: FlexFit.loose,
+               child:Image.network("https://previews.123rf.com/images/pornpatmalajak/pornpatmalajak1601/pornpatmalajak160100059/51331132-quote-about-love-and-coffee-on-coffee-background-design-with-vintage-style-valentine-s-day-.jpg")
 
+             ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(FontAwesomeIcons.heart),
+                      16.widthBox,
+                      Icon(FontAwesomeIcons.comment),
+                      16.widthBox,
+                      Icon(FontAwesomeIcons.paperPlane)
+                    ],
+                  ),
+                  Icon(FontAwesomeIcons.bookmark)
+                ],
+              ),
+            ),
+           "Liked By Priya,sheha and 568231 Others".text.make()
           ],
         ),
     );
